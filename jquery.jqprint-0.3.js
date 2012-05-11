cc1d780998dd2567457546ae744ddad8cd49e808
Mark Howells-Mead <mark@Pluto.local>
2012-05-11 14:45:02 +0200

    v0.4
    Improved implementation of CSS file referencing.

--- jquery.jqprint-0.3.js
+++ jquery.jqprint-0.3.js
@@ -1,7 +1,8 @@
 ﻿// -----------------------------------------------------------------------
 // Eros Fratini - eros@recoding.it
-// jqprint 0.3
+// jqprint 0.4
 //
+// - 11/05/2012 - improved implementation of css file inclusion (mark@permanenttourist.ch)
 // - 19/06/2009 - some new implementations, added Opera support
 // - 11/05/2009 - first sketch
 //
@@ -18,52 +19,47 @@
         opt = $.extend({}, $.fn.jqprint.defaults, options);
 
         var $element = (this instanceof jQuery) ? this : $(this);
-        
-        if (opt.operaSupport && $.browser.opera) 
-        { 
+
+        if (opt.operaSupport && $.browser.opera)
+        {
             var tab = window.open("","jqPrint-preview");
             tab.document.open();
 
             var doc = tab.document;
         }
-        else 
+        else
         {
             var $iframe = $("<iframe  />");
-        
+
             if (!opt.debug) { $iframe.css({ position: "absolute", width: "0px", height: "0px", left: "-600px", top: "-600px" }); }
 
             $iframe.appendTo("body");
             var doc = $iframe[0].contentWindow.document;
         }
-        
+
         if (opt.importCSS)
         {
-            if ($("link[media=print]").length > 0) 
-            {
-                $("link[media=print]").each( function() {
-                    doc.write("<link type='text/css' rel='stylesheet' href='" + $(this).attr("href") + "' media='print' />");
-                });
-            }
-            else 
-            {
-                $("link").each( function() {
-                    doc.write("<link type='text/css' rel='stylesheet' href='" + $(this).attr("href") + "' />");
-                });
-            }
+			$("link[rel=stylesheet]").each(function(){
+           		var href = $(this).attr('href');
+           		if(href){
+					var media = $(this).attr('media') || 'all';
+					doc.write("<link type='text/css' rel='stylesheet' href='" + href + "' media='"+media+"'>");
+				}
+            });
         }
-        
+
         if (opt.printContainer) { doc.write($element.outer()); }
         else { $element.each( function() { doc.write($(this).html()); }); }
-        
+
         doc.close();
-        
+
         (opt.operaSupport && $.browser.opera ? tab : $iframe[0].contentWindow).focus();
         setTimeout( function() { (opt.operaSupport && $.browser.opera ? tab : $iframe[0].contentWindow).print(); if (tab) { tab.close(); } }, 1000);
     }
-    
+
     $.fn.jqprint.defaults = {
 		debug: false,
-		importCSS: true, 
+		importCSS: true,
 		printContainer: true,
 		operaSupport: true
 	};
@@ -71,5 +67,5 @@
     // Thanks to 9__, found at http://users.livejournal.com/9__/380664.html
     jQuery.fn.outer = function() {
       return $($('<div></div>').html(this.clone())).html();
-    } 
+    }
 })(jQuery);
