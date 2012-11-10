9c4ec2d837abd08984d6cbfefa911d3bc178a166
Jason Day <movemaine@gmail.com>
2012-11-09 20:57:18 -0500

    Update jquery.printThis.js
    reverted change.

--- jquery.printThis.js
+++ jquery.printThis.js
@@ -1,116 +1,91 @@
 // -----------------------------------------------------------------------
-// Print This
+// printThis v1.0
+// Printing plug-in for jQuery
 //
-// Based on jPrintArea: http://plugins.jquery.com/project/jPrintArea
-// and jqprint: https://github.com/tanathos/jquery.jqprint
+// Resources (based on) :
+//              jPrintArea: http://plugins.jquery.com/project/jPrintArea
+//              jqPrint: https://github.com/permanenttourist/jquery.jqprint
+//              Ben Nadal: http://www.bennadel.com/blog/1591-Ask-Ben-Print-Part-Of-A-Web-Page-With-jQuery.htm
+//
+// Dual licensed under the MIT and GPL licenses:
+//              http://www.opensource.org/licenses/mit-license.php
+//              http://www.gnu.org/licenses/gpl.html
+//
+// (c) Jason Day 2012
+//
 //------------------------------------------------------------------------
 
-(function ($) {
-	$.fn.printThis = function (options) {
-		//Merge defaults and options
-		options = $.extend({}, $.fn.printThis.defaults, options);
-		
-		if($.browser.opera) {
-			$.printThis.operaBrowser.call(this, options);
-		}
-		else {
-			$.printThis.allBrowsers.call(this, options);
-		}
-		
-		//return for chainability
-		return this;
-	};
 
-	//private methods
-	$.printThis = {
-		//returns the String for the given options
-		_getCSSStyles: function (element, options) {
-			//get jqeury dom element
-			var $element = (element instanceof jQuery) ? element : $(element),
-				_styleStr = ''; //holds the style/html string
+(function($) {
+    var opt;
 
-			//if importCss is true then get the style sheet links from page
-			if(options.importCSS) {
-				$("link[rel=stylesheet]").each(function () {
-					var $this = $(this),
-						_href = $this.attr('href');
-					if(_href) {
-						var _media = $this.attr('media') || 'all';
-						_styleStr += "<link type='text/css' rel='stylesheet' href='" + _href + "' media='" + _media + "'>";
-					}
-				});
-			}
+    $.fn.printThis = function (options) {
+        opt = $.extend({}, $.fn.printThis.defaults, options);
 
-			//append external link if available
-			if(options.loadCSS) {
-				_styleStr += "<link type='text/css' rel='stylesheet' href='" + options.loadCSS + "'>";
-			}
+        var $element = (this instanceof jQuery) ? this : $(this);
 
-			//
-			if(options.printContainer) {
-				_styleStr += $element.outer();
-			}
-			else {
-				$element.each(function () {
-					_styleStr += $(this).html();
-				});
-			}
+        if ($.browser.opera)
+        {
+            var tab = window.open("","Print Preview");
+            tab.document.open();
 
-			return _styleStr;
-		},
+            var doc = tab.document;
+        }
+        else
+        {
+	    var strFrameName = ("printThis-" + (new Date()).getTime());
+	    
+            var $iframe = $("<iframe id=" + strFrameName +"/>");
 
-		//Print functionality for Opera Browsers ONLY, not using any iframe
-		operaBrowser: function (options) {
-			var tab = window.open("", "Print Preview");
-			tab.document.open();
-			var doc = tab.document;
-			doc.write($.printThis._getCSSStyles(this, options));
-			doc.close();
-			tab.focus();
-			setTimeout(function () {
-				tab.print();
-				tab.close();
-			}, 1000);
-		},
+            if (!opt.debug) { $iframe.css({ position: "absolute", width: "0px", height: "0px", left: "-600px", top: "-600px" }); }
 
-		//print functionality for all other browsers except opera
-		allBrowsers: function (options) {
-			//get unique iframe name
-			var strFrameName = ("printThis-" + (new Date()).getTime()),
-				//holds style/html string 
-				styleStr = $.printThis._getCSSStyles(this, options);
-			
-			//create an iframe
-            var $iframe = $("<iframe name=" + strFrameName +"/>");
-			
-            if (!options.debug) { $iframe.css({ position: "absolute", width: "0px", height: "0px", left: "-600px", top: "-600px" }); }
-			//append iframe to body
             $iframe.appendTo("body");
-			//append stylestr to iframe
-            $($iframe[0].contentWindow.document).find('body').append(styleStr);
-			
-			$iframe[0].contentWindow.focus();
-			setTimeout(function () {					
-					$iframe[0].contentWindow.print();
-			}, 1000);
+            var doc = $iframe[0].contentWindow.document;
+        }
 
-			//removed iframe after 60 seconds
-			setTimeout(
-			function () {
-				$iframe.remove();
-			}, (60 * 1000));
-		}
-	};
+        if (opt.importCSS)
+        {
+			$("link[rel=stylesheet]").each(function(){
+           		var href = $(this).attr('href');
+           		if(href){
+					var media = $(this).attr('media') || 'all';
+					doc.write("<link type='text/css' rel='stylesheet' href='" + href + "' media='"+media+"'>");
+				}
+            });
+        }
+	
+	if (opt.loadCSS)
+	{
+	    doc.write("<link type='text/css' rel='stylesheet' href='" + opt.loadCSS + "'>");
+	    
+	}
 
-	// Default settings
-	$.fn.printThis.defaults = {
+        if (opt.printContainer) { doc.write($element.outer()); }
+        else { $element.each( function() { doc.write($(this).html()); }); }
+
+        doc.close();
+
+        ($.browser.opera ? tab : $iframe[0].contentWindow).focus();
+        setTimeout( function() { ($.browser.opera ? tab : $iframe[0].contentWindow).print(); if (tab) { tab.close(); } }, 1000);
+	
+	//removed iframe after 60 seconds
+	setTimeout(
+	    function(){
+	    $iframe.remove();
+	    },
+	    (60 * 1000)
+	    );
+    }
+
+    $.fn.printThis.defaults = {
 		debug: false,
 		importCSS: true,
 		printContainer: true,
 		loadCSS: ""
 	};
 
-	jQuery.fn.outer = function () {
-		return $($('<div></div>').html(this.clone())).html();
-	}
+    
+    jQuery.fn.outer = function() {
+      return $($('<div></div>').html(this.clone())).html();
+    }
 })(jQuery);
