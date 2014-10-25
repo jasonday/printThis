25fdd4c7d43ee85f731c9b0174d1c9eeb69ce63e
Jason Day <movemaine@gmail.com>
2014-10-25 00:06:08 -0400

    Update printThis.js

--- printThis.js
+++ printThis.js
@@ -20,7 +20,7 @@
  *      importCSS: true,            * import page CSS
  *      importStyle: true,          * import style tags
  *      printContainer: true,       * grab outer container as well as the contents of the selector
- *      loadCSS: "path/to/my.css",  * path to additional css file
+ *      loadCSS: "path/to/my.css",  * path to additional css file - us an array [] for multiple
  *      pageTitle: "",              * add title to print page
  *      removeInline: false,        * remove all inline styles from print elements
  *      printDelay: 333,            * variable print delay
