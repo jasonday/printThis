f116f81eb2a6d5829d0b8ecf4a834294ed7e9ab3
Samuel Rouse <samuel.rouse@gmail.com>
2018-09-03 20:24:05 -0400

    Add Utilities, update to export

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,4 @@
-function isOldIE() {
+const isOldIE = () => {
   const isBrowser = typeof navigator !== "undefined";
 
   if (!isBrowser) {
@@ -6,10 +6,33 @@ function isOldIE() {
   }
 
   return /msie/i.test(navigator.userAgent);
-}
+};
 
-function getNow() {
-  return isOldIE() ? new Date().getTime() : Date.now();
-}
+const getNodes = (selector, fragment = document) => {
+  // Easy path first
+  if (util.hasDSA()) {
+    return Array.from(fragment.querySelectorAll(seletor));
+  }
 
-export { getNow, isOldIE };
+  /*
+   * TODO: Add jQuery support. Can we do this in a way that doesn't use the global?
+   * Perhaps pass jQuery to this function from the jQuery plugin?
+   */
+
+  // Error out if we can't run.
+  throw new Error('Support is limited to recent browsers. https://caniuse.com/#feat=queryselector');
+};
+
+const getNow = () => Date.now === undefined ? new Date().getTime() : Date.now();
+
+const hasDSA = () => !!(document && document.querySelectorAll);
+
+const removeNode = node => node.parentNode && node.parentNode.removeChild(node);
+
+export {
+  getNodes,
+  getNow,
+  hasDSA,
+  isOldIE,
+  removeNode,
+};
