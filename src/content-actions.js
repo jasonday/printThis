88946fde90aa174b75155d24062a46e4df33bf65
Samuel Rouse <samuel.rouse@gmail.com>
2018-09-16 13:56:46 -0400

    Add content actions

--- src/content-actions.js
+++ src/content-actions.js
@@ -1,7 +1,14 @@
+import * as addBaseTag from './actions/addBaseTag';
+import * as addStyles from './actions/addStyles';
+import * as addTitle from './actions/addTitle';
 import * as removeInlineStyles from "./actions/removeInlineStyles";
 import * as removeScripts from "./actions/removeScripts";
 
+
 const modules = [
+  addBaseTag,
+  addStyles,
+  addTitle,
   removeInlineStyles,
   removeScripts,
 ];
