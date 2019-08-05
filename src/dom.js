3b1265ee31424f7613e92c65b8182c94f07ee0d5
Samuel Rouse <samuel.rouse@gmail.com>
2019-08-04 23:56:57 -0400

    Work on element copy functions

--- src/dom.js
+++ src/dom.js
@@ -18,6 +18,12 @@ const copyContent = (content, withFormData) => {
 
   // If Node, deep clone
   if (content instanceof Node) {
+    if (content instanceof HTMLCanvasElement) {
+      const srcData = content.getImageData(0, 0, content.canvas.width, content.canvas.height);
+      const newNode = content.cloneNode(true);
+      newNode.getContext("2d").drawImage(content, 0, 0);
+      return newNode;
+    }
     return content.cloneNode(true);
   }
 
@@ -25,4 +31,7 @@ const copyContent = (content, withFormData) => {
   return undefined;
 };
 
+const cloneCanvas = (node) => {
+  // TODO - canvas needs to find canvas entries and match source and destination..
+};
 
